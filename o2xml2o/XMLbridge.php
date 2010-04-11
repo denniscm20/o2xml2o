44dcc60e43085865bcddc5992d87642477b34e90
denniscm20 <denniscm20@2b618c08-176a-11de-b208-e5a9466b8568>
2010-04-11 01:11:07 +0000

    Project rebuild

--- o2xml2o/XMLbridge.php
+++ o2xml2o/XMLbridge.php
@@ -1,7 +1,7 @@
 <?php
 
 /************************************************************************
-        XMLserializer - Copyright 2009 Dennis Cohn Muroy
+        XMLbridge - Copyright 2009 Dennis Cohn Muroy
 
 This file is part of o2xml2o.
 
@@ -24,7 +24,7 @@ along with o2xml2o.  If not, see <http://www.gnu.org/licenses/>.
  * This class allows any child class to be exported into an XML structure or
  * allows it to import its attributes' values from a previously exported
  * XML file.
- * This class is the XML (un)serializer for your objects!!!
+ * This class is the XML bridge for your objects!!!
  * @author Dennis Cohn Muroy <dennis.cohn@pucp.edu.pe>
  * @copyright Copyright (c) 2009, Dennis Cohn Muroy
  * @version 0.5
@@ -32,7 +32,7 @@ along with o2xml2o.  If not, see <http://www.gnu.org/licenses/>.
  * @package o2xml2o
  * @abstract
  */
-abstract class XMLserializer
+abstract class XMLbridge
 {
     /**
      * Converts an object into a XML Structure
@@ -81,11 +81,10 @@ abstract class XMLserializer
                 $type = "Value";
                 $class = gettype($value);
             }
-            $keyTag = is_numeric($key)?"index_".$key:$key;
-            print "<{$keyTag} type=\"{$type}\" class=\"{$class}\">\n";
+            print "<element name=\"{$key}\" type=\"{$type}\" class=\"{$class}\">\n";
             $type = $type."XML";
             $this->{$type}($value);
-            print "</{$keyTag}>";
+            print "</element>";
         }
     }
 
@@ -102,12 +101,11 @@ abstract class XMLserializer
         $attributes = get_object_vars($this);
         if ($writeHeader) {
             header ("content-type: text/xml");
-            $objectName = get_class($this);
-            print ("<{$objectName}>\n");
+            print ("<elements>\n");
         }
         $this->arrayXML($attributes);
         if ($writeHeader) {
-            print ("</{$objectName}>");
+            print ("</elements>");
         }
     }
 
@@ -124,8 +122,7 @@ abstract class XMLserializer
         foreach ($node->children() as $element) {
             $attributes = $element->attributes();
             $type = $attributes['type'];
-            $name = $element->getName();
-            $name = str_replace("index_","",$name);
+            $name = $attributes['name'];
             $class = (string)$attributes['class'];
             switch ($type) {
                 case "Object":  $object = new $class();
@@ -133,7 +130,7 @@ abstract class XMLserializer
                                 $innerArray["{$name}"] = $object;
                                 break;
                 case "Array":   $innerArray["{$name}"] = $this->getArrayStructure($element); break;
-                case "Value":   $innerArray["{$name}"] = trim((string)$element); break;
+                case "Value":   $innerArray["{$name}"] = (string)$element; break;
             }
         }
         return $innerArray;
@@ -151,11 +148,11 @@ abstract class XMLserializer
         foreach ($node->children() as $element) {
             $attributes = $element->attributes();
             $type = $attributes['type'];
-            $name = $element->getName();
+            $name = $attributes['name'];
             switch ($type) {
                 case "Object": $this->{$name}->readStructure($element); break;
                 case "Array": $this->{$name} = $this->getArrayStructure($element); break;
-                case "Value": $this->{$name} = trim((string)$element); break;
+                case "Value": $this->{$name} = (string)$element; break;
             }
         }
     }
