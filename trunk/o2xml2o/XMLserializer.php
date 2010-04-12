4cf74e3ab38bf9cd08d3088830f1203553bc5dea
denniscm20 <denniscm20@2b618c08-176a-11de-b208-e5a9466b8568>
2010-04-12 01:19:00 +0000

    Refactor was applied to this class. The methods were renamed in order to make the code easy to understand and modify.

--- trunk/o2xml2o/XMLserializer.php
+++ trunk/o2xml2o/XMLserializer.php
@@ -26,88 +26,110 @@ along with o2xml2o.  If not, see <http://www.gnu.org/licenses/>.
  * XML file.
  * This class is the XML (un)serializer for your objects!!!
  * @author Dennis Cohn Muroy <dennis.cohn@pucp.edu.pe>
- * @copyright Copyright (c) 2009, Dennis Cohn Muroy
- * @version 0.5
+ * @copyright Copyright (c) 2010, Dennis Cohn Muroy
+ * @version 0.9
  * @license http://opensource.org/licenses/gpl-3.0.html GNU Public License 3
  * @package o2xml2o
  * @abstract
  */
 abstract class XMLserializer
 {
+    const OPEN_TAG = "<%s type=\"%s\" class=\"%s\">\n";
+    const CLOSE_TAG = "</%s>\n";
+
     /**
-     * Converts an object into a XML Structure
+     * Exports a list of elements into XML
      * @author Dennis Cohn Muroy
      * @access private
-     * @final
-     * @param Object $value Object to be converted to an XML structure
+     * @param array $elements List of elements to be exported into XML
      */
-    private function objectXML($value)
+    private function printElements($elements)
     {
-        $funct = "writeXML"; 
-        if (is_callable(array($value,$funct),true) !== FALSE) {
-            $value->{$funct}(false);
+        foreach ($elements as $key => $value)
+        {
+            $keyTag = is_numeric($key)?"index_".$key:$key;
+            if (is_object($value)) {
+                $this->printObject($value, $keyTag);
+            } else if (is_array($value)) {
+                $this->printArray($value, $keyTag);
+            } else {
+                $this->printValue($value, $keyTag);
+            }
         }
+    }
+
+    /**
+     * Exports an object into a XML Structure
+     * @author Dennis Cohn Muroy
+     * @access private
+     * @param Object $object Object to be exported to XML
+     * @param string $attributeName Name of the attribute where the $object is
+     * assigned.
+     */
+    private function printObject($object, $attributeName)
+    {
+        $class = get_class($object);
+        $attributes = get_object_vars($object);
+        echo sprintf(self::OPEN_TAG, $attributeName, 'Object', $class);
+        $this->printElements($attributes);
+        echo sprintf(self::CLOSE_TAG, $attributeName);
     }
 
     /**
      * Writes the value of a XML node.
      * @author Dennis Cohn Muroy
      * @access private
-     * @param Object $value Value to be displayed in a XML node
+     * @param string|integer|boolean $value Value to be exported to a XML node
+     * @param string $attributeName Name of the attribute where the $value is
+     * assigned.
      */
-    private function valueXML($value)
+    private function printValue($value, $attributeName)
     {
-        print "{$value}\n";
+        $class = gettype($value);
+        echo sprintf(self::OPEN_TAG, $attributeName, 'Value', $class);
+        echo $value;
+        echo sprintf(self::CLOSE_TAG, $attributeName);
     }
 
     /**
-     * Converts a list of attributes with is assigned values into XML nodes
+     * Exports a list of elements to XML
      * @author Dennis Cohn Muroy
      * @access private
-     * @param mixed $elements List of attributes with its assigned values
+     * @param array $array List of elements to be exported to XML.  These
+     * elements can be Objects, arrays or basic datatypes.
      */
-    private function arrayXML($elements)
+    private function printArray($array, $attributeName)
     {
-        foreach($elements as $key => $value) {
-            $type = "";
-            $class = "";
-            if (is_object($value)) {
-                $type = "Object";
-                $class = get_class($value);
-            } else if (is_array($value)) {
-                $type = "Array";
-                $class = "array";
-            } else {
-                $type = "Value";
-                $class = gettype($value);
-            }
-            $keyTag = is_numeric($key)?"index_".$key:$key;
-            print "<{$keyTag} type=\"{$type}\" class=\"{$class}\">\n";
-            $type = $type."XML";
-            $this->{$type}($value);
-            print "</{$keyTag}>";
-        }
+        echo sprintf(self::OPEN_TAG, $attributeName, 'Array', 'array');
+        $this->printElements($array);
+        echo sprintf(self::CLOSE_TAG, $attributeName);
     }
 
     /**
-     * This is the method that must be called in order to convert a child object
-     * into an XML structure.
+     * Returns the value that will be assigned to the object attribute
      * @author Dennis Cohn Muroy
-     * @access public
-     * @final
-     * @param bool $writeHeader Indicates if the xml header must be written.
+     * @access private
+     * @param mixed $element Element that will be imported from XML into an
+     * Object, Array or Basic Datatype variable.
+     * @param string $type Indicates if the $element is an Object, an Array or
+     * a basic datatype.
+     * @param string $class Class of the object that was previously exported
+     * into XML.
+     * @return mixed Value to be assigned to an object attribute.
      */
-    public final function writeXML($writeHeader = true)
+    private function readElement($element, $type, $class)
     {
-        $attributes = get_object_vars($this);
-        if ($writeHeader) {
-            header ("content-type: text/xml");
-            $objectName = get_class($this);
-            print ("<{$objectName}>\n");
-        }
-        $this->arrayXML($attributes);
-        if ($writeHeader) {
-            print ("</{$objectName}>");
+        switch ($type) {
+            case "Object":
+                $object = new $class();
+                if (method_exists($object, "readStructure")) {
+                    $object->readStructure($element);
+                }
+                return $object;
+            case "Array":
+                return $this->readArrayStructure($element);
+            case "Value":
+                return (string)trim($element);
         }
     }
 
@@ -118,23 +140,19 @@ abstract class XMLserializer
      * @param SimpleXMLElement $node Node of the XML Structure
      * @return array
      */
-    private function getArrayStructure($node)
+    private function readArrayStructure($node)
     {
         $innerArray = array();
         foreach ($node->children() as $element) {
             $attributes = $element->attributes();
             $type = $attributes['type'];
             $name = $element->getName();
+            /**
+             * @todo It must replace only the first occurrence
+             */
             $name = str_replace("index_","",$name);
             $class = (string)$attributes['class'];
-            switch ($type) {
-                case "Object":  $object = new $class();
-                                $object->readStructure($node->children());
-                                $innerArray["{$name}"] = $object;
-                                break;
-                case "Array":   $innerArray["{$name}"] = $this->getArrayStructure($element); break;
-                case "Value":   $innerArray["{$name}"] = trim((string)$element); break;
-            }
+            $innerArray["{$name}"] = $this->readElement($element, $type, $class);
         }
         return $innerArray;
     }
@@ -152,17 +170,14 @@ abstract class XMLserializer
             $attributes = $element->attributes();
             $type = $attributes['type'];
             $name = $element->getName();
-            switch ($type) {
-                case "Object": $this->{$name}->readStructure($element); break;
-                case "Array": $this->{$name} = $this->getArrayStructure($element); break;
-                case "Value": $this->{$name} = trim((string)$element); break;
-            }
+            $class = (string)$attributes['class'];
+            $this->{$name} = $this->readElement($element, $type, $class);
         }
     }
 
     /**
      * This is the method that must be called in order to convert an XML
-     * structure into an object.
+     * structure into an object that extends the XMLbridge class.
      * @author Dennis Cohn Muroy
      * @access public
      * @final
@@ -179,6 +194,19 @@ abstract class XMLserializer
         }
         $xml = new SimpleXMLElement($file, NULL, TRUE);
         $this->readStructure($xml);
+    }
+
+    /**
+     * This is the method that must be called in order to export the child object
+     * into an XML structure.
+     * @author Dennis Cohn Muroy
+     * @access public
+     * @final
+     */
+    public final function writeXML()
+    {
+        header ("content-type: text/xml");
+        $this->printObject($this, get_class($this));
     }
 }
 
